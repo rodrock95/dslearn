caa9f446ba70d9d92b6b00886a9ac4d4f4847fd2
rodrock95 <r.barboza9502@gmail.com>
2022-05-12 20:19:49 -0300

    Offer

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -0,0 +1,103 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+	private static final long serialVersionUID = 1L; 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
+	
+	@ManyToOne //muitos para um
+	@JoinColumn(name = "course_id")
+	private Course course; //uma oferta tem um curso
+	
+	public Offer() {
+	}
+
+	public Offer(int id, String edition, Instant startMoment, Instant endMoment, Course course) {
+		super();
+		this.id = id;
+		this.edition = edition;
+		this.startMoment = startMoment;
+		this.endMoment = endMoment;
+		this.course = course;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public Instant getStartMoment() {
+		return startMoment;
+	}
+
+	public void setStartMoment(Instant startMoment) {
+		this.startMoment = startMoment;
+	}
+
+	public Instant getEndMoment() {
+		return endMoment;
+	}
+
+	public void setEndMoment(Instant endMoment) {
+		this.endMoment = endMoment;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Offer other = (Offer) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
